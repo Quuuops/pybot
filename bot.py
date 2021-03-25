56a72f864ef84d96a413b92e271201f5949b64c6
artur <artur7borisovskiy@gmail.com>
2021-03-25 17:52:40 +0200

    2 version

--- bot.py
+++ bot.py
@@ -1,62 +1,84 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime, timedelta, date, time
 from random import randrange
+
+from telegram.ext import Updater, CommandHandler
+
 import db
-from datetime import datetime,timedelta
+
 print("Бот запущен. Нажмите Ctrl+C для завершения")
+
+
+def show_top(update, context):
+    chat = update.effective_chat
+    growth_users = db.database.select_top(chat.id)
+    msg = 'Топ бутылок \n'
+    for i in range(len(growth_users)):
+        msg += '{}. @{} : {} см\n'.format(i+1,growth_users[i]['user'], growth_users[i]['growth'])
+    context.bot.send_message(chat.id, text=msg)
+
+def now_time():
+    return time(00)
 
 
 def palma(update, context):
     chat = update.effective_chat
     rand = randrange(-5, 15)
-    curent_growth = db.database.select(update.message.from_user['id'])
-
-    if curent_growth == None :
+    curent_growth = db.database.select(update.message.from_user['id'],chat_id=chat.id)
+    if curent_growth == None:
         print(update.message.from_user['username'], 'Добавлен')
         db.database.add(name=update.message.from_user['username'], first_name=update.message.from_user['first_name'],
                         user_id=
-                        update.message.from_user['id'], count=0,)
-        db.database.update(user_id=update.message.from_user['id'],date=datetime.now()+ timedelta(days=1), palma=rand)
-        curent_growth_now = db.database.select(update.message.from_user['id'])
-
+                        update.message.from_user['id'], count=0, chat_id=chat.id)
+        db.database.update(user_id=update.message.from_user['id'],
+                           date=datetime.combine(date.today() + timedelta(1), now_time()), palma=rand)
+        curent_growth_now = db.database.select(update.message.from_user['id'],chat_id=chat.id)
 
         if rand < 0:
 
             context.bot.send_message(chat_id=chat.id, text="Ваша бутылка сократилась на {} \n"
                                                            "Ваша бутылка {} сантиметров".format(rand,
-                                                                                                curent_growth_now['growth']))
+                                                                                                curent_growth_now[
+                                                                                                    'growth']))
         else:
             context.bot.send_message(chat_id=chat.id, text="Ваша бутылка выросла на {} \n"
                                                            "Ваша бутылка {} сантиметров".format(rand,
 
-                                                                                                curent_growth_now['growth']))
+                                                                                                curent_growth_now[
+                                                                                                    'growth']))
     else:
-        curent_user =db.database.select(update.message.from_user['id'])
-        if curent_user['date'] < datetime.now() :
-            db.database.update(user_id=update.message.from_user['id'], date=datetime.now()+ timedelta(days=1), palma=rand)
+        curent_user = db.database.select(update.message.from_user['id'],chat.id)
+        if curent_user['date'] < datetime.now():
+            db.database.update(user_id=update.message.from_user['id'],
+                               date=datetime.combine(date.today() + timedelta(1), now_time()),
+                               palma=rand)
             curent_growth_now = db.database.select(update.message.from_user['id'])
 
             if rand < 0:
 
                 context.bot.send_message(chat_id=chat.id, text="Ваша бутылка сократилась на {} \n"
                                                                "Ваша бутылка {} сантиметров".format(rand,
-                                                                                                    curent_growth_now['growth']))
+                                                                                                    curent_growth_now[
+                                                                                                        'growth']))
             else:
                 context.bot.send_message(chat_id=chat.id, text="Ваша бутылка выросла на {} \n"
                                                                "Ваша бутылка {} сантиметров".format(rand,
-                                                                                                    curent_growth_now['growth']))
+                                                                                                    curent_growth_now[
+                                                                                                        'growth']))
         else:
-            curent_growth_user = db.database.select(update.message.from_user['id'])
+            curent_growth_user = db.database.select(update.message.from_user['id'],chat_id=chat.id)
             date_user_db = curent_growth_user['date']
             date_now = datetime.now()
-            time = date_user_db-date_now
+            time = date_user_db - date_now
             minute = (time.seconds // 60) % 60
             hours = (time.seconds / 60) // 60
-            context.bot.send_message(chat_id=chat.id, text="Попробуй через {} ч {} м  ".format(int(hours),minute))
+            print(chat.id)
+            context.bot.send_message(chat_id=chat.id, text="Попробуй через {} ч {} м  ".format(int(hours), minute))
 
 
 updater = Updater('', use_context=True)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("bottle", palma))
+dispatcher.add_handler(CommandHandler("top10", show_top))
 
 updater.start_polling()
 updater.idle()
--- db.py
+++ db.py
@@ -6,7 +6,7 @@ class Database:
         host='localhost',
         user='',
         password='',
-        db='tgbot',
+        db='',
         charset='utf8',
         cursorclass=pymysql.cursors.DictCursor
     )
@@ -15,20 +15,20 @@ class Database:
     else:
         print('No connection!')
 
-    def select(self, user_id, ):
+    def select(self, user_id, chat_id):
         with self.connection.cursor() as cursor:
-            query = "SELECT * FROM palmabot WHERE `user_id` = %s "
-            cursor.execute(query, (user_id))
+            query = "SELECT * FROM palmabot WHERE `user_id` = %s AND `chat_id` = %s"
+            cursor.execute(query, (user_id,chat_id))
             row = cursor.fetchall()
             if len(row) > 0:
                 return row[0]
             else:
                 return None
 
-    def add(self, name, first_name, count, user_id):
+    def add(self, name, first_name, count, user_id,chat_id):
         with self.connection.cursor() as cursor:
-            query = "INSERT INTO palmabot (`user`, `first_name` , `growth`, `user_id`) VALUES (%s, %s, %s, %s)"
-            cursor.execute(query, (name, first_name, count, user_id))
+            query = "INSERT INTO palmabot (`user`, `first_name` , `growth`, `user_id`, `chat_id`) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(query, (name, first_name, count, user_id,chat_id))
             self.connection.commit()
 
     def update(self, user_id,date, palma):
@@ -36,6 +36,14 @@ class Database:
             query = "UPDATE palmabot SET growth = growth + %s , date = %s WHERE user_id = %s"
             cursor.execute(query, (palma, date, user_id))
             self.connection.commit()
+
+    def select_top(self,chat_id):
+        with self.connection.cursor() as cursor:
+            query= "SELECT * FROM palmabot WHERE chat_id = %s ORDER BY `growth` DESC"
+            cursor.execute(query, (chat_id))
+            self.connection.commit()
+            row = cursor.fetchall()
+            return row
 
 
 database = Database()
